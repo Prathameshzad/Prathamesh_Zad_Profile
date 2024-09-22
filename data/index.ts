d897240bec786e83e382faa45d971d1d25f5803c
Prathameshzad <prathameshzad20@gmail.com>
2024-09-22 22:39:47 +0530

    final comment

---
+++ data/index.ts
@@ -0,0 +1,243 @@
+export const aboutItems = [
+  {
+    id: 1,
+    title: "Frontend Developer with 1 years of experience",
+    description: "",
+    img: "/b1.jpg",
+    spareImg: ""
+  },
+  {
+    id: 2,
+    title: "Working on both CST and IST timezones",
+    description: "",
+    img: "",
+    spareImg: ""
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "Constantly learing new technologies",
+    img: "/grid.svg",
+    spareImg: ""
+  },
+  {
+    id: 4,
+    title: "Working hard to be a Lead",
+    description: "",
+    img: "/grid.svg",
+    spareImg: "/b4.svg"
+  },
+
+  {
+    id: 5,
+    title: "Currently learning DevOps technologies",
+    description: "Behind the Scene",
+    img: "/b5.webp",
+    spareImg: "/grid.svg"
+  },
+  {
+    id: 6,
+    title: "Want to reach out to me ?",
+    description: "",
+    img: "",
+    spareImg: ""
+  }
+];
+
+export const freelancing = [
+  {
+    title: "Alvyx",
+    description:
+      "It's an International Luxury Clothing Brand which as Dashboard and recommandation System. This is currently on going Application.",
+    link: "https://alvyx.com/",
+    img: "/alvyx.png"
+  },
+  {
+    title: "Sinremtech",
+    description:
+      "This was my Internship Company website which was code my me as they provide me the figma file for it.",
+    link: "https://sinremtech.com/",
+    img: "/sinremtech.png"
+  },
+  {
+    title: "Shree Metal Industries",
+    description:
+      "This was website was design as per the client request which also has a dashboard for adding products.",
+    link: "https://shreemetalind.in/product.html",
+    img: "/steelindustry.png"
+  },
+  {
+    title: "CVK Engineers",
+    description:
+      "This website was developed in my Internship as this was not the final website I developed it is changed by the company.",
+    link: "https://cvkengineers.in/",
+    img: "/cvkengineer.png"
+  },
+
+  {
+    title: "Malhar Powertronics",
+    description:
+      "This Website is design as per the client request which has dashboard too where the client can add Events, Products.",
+    link: "https://malharpowertronicsllp.com/",
+    img: "/malhar.png"
+  },
+]
+
+export const projects = [
+  {
+    title: "Ochi Design",
+    description:
+      "A clone of Ochi Design homepage using React.js, Tailwind CSS, and various npm packages.",
+    link: "https://ochi-design-ruddy.vercel.app/",
+    img: "/ochidesign.png"
+  },
+  {
+    title: "True Feedback",
+    description:
+      "Developed a full stack application allowing users to receive anonymous messages via a shared URL. Using TypeScript, JavaScript,  Next.js, NextAuth, MongoDB and ShadcnUI",
+    link: "https://github.com/Prathameshzad/true-feedback",
+    img: "/truefeedback.png"
+  },
+  {
+    title: "AutoSculpt",
+    description:
+      "A 3D website for Race lover using simple technology. Mostly Focus on the web responsive UI.",
+    link: "https://autoscuplt.vercel.app/",
+    img: "/carrace.png"
+  },
+  {
+    title: "Spidey",
+    description:
+      "A Website for Spiderman lover using simple technology.Mostly Focus on the web responsive UI..",
+    link: "https://spidey-nine.vercel.app/",
+    img: "/spiderman.png"
+  },
+
+  {
+    title: "Vessels",
+    description:
+      "Highly focus on UI for this website and also note that this is website responsive only",
+    link: "https://vessels-weld.vercel.app/",
+    img: "/hario.png"
+  },
+
+  // {
+  //   title: "Job Search - React Native",
+  //   description:
+  //     "A React Native mobile app utilizing Rapid API to list job vacancies, built with Expo.",
+  //   link: "https://vimeo.com/830133436?turnstile=0.p1b86vFiHapG8c4374Fb_wZr8jBX2JJdvp45f4KYo_sVSwl2qPqSpEEYVeYzwovkjNEJwOq2IOFElC4QeoZDGmjlx90Tcp0PpvyIuwet3qL3YYZdEobwceyHWkBmwaTkAbcQHJRgq52uz4Sv6RZIhGYDN3wqKpCJKkvu_zVN0EYHEomlM0tOalrwG3qaLOFOwRwF7j9Doe1RndqhuG52l4v4z-FkRmFUZjJ_IcmJt8hdyIVW28SdOKeLdpMh2Qo9so3zqWD7PSSbfeh33z27GYbMaLvXaksWoZO6YCeXlbXeAehrDQFvFsyDSKFVS-_tSez7PIkE21uE5rDp1guPw3SbdH_jXhzF1HaA1UDZAgjSsCCSTnl5yakLULIs9NE0IL9d8ta1IIF_kKsrLSlq7n_60Rxf-MlRT-UNWFMfDrAkO-z-Nz9sgI2w8qoFM9PJ.dXsJfsC4IS-o3TiFA7XECw.67ad233e530bcb2da88764b22371c0ed4bf981f3b6ac216f1ac7a55513f7dcc9",
+  //   img: "/jobsearch.png"
+  // }
+];
+
+export const experiences = [
+  {
+    title: "Freelancer",
+    company: "Self-employed",
+    img: "/xs.jpeg",
+    year: "Currently",
+    companyLink: "",
+    desc: "Developed and maintained full-stack web applications using the MERN (MongoDB, Express.js, React.js, Node.js) stack."
+  },
+  {
+    title: "Frontend Developer - Intern",
+    company: "Sinremtech",
+    img: "/sinremtechlogo.png",
+    year: "Mar, 2024",
+    companyLink: "https://sinremtech.com/",
+    desc: "Managed all front-end projects, identifying and fixing design errors to ensure a seamless user experience."
+  },
+  {
+    title: "Software Engineer - Co-Founder",
+    company: "Batch_Mates",
+    img: "/badgemate.png",
+    year: "Sept 2023",
+    companyLink: "https://www.linkedin.com/company/batch-mates/posts/?feedView=all",
+    desc: "Analyzed customer design needs and created tailored designs to meet specific requirements"
+  },
+  {
+    title: "Jr. QA Engineer",
+    company: "Zad Digital Solution",
+    img: "/j4s.png",
+    year: "Dec, 2022",
+    companyLink: "",
+    desc: "Produced bug reports to improve issue resolution and enhance communication between testing and development teams"
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I have worked with Koustav on many projects during which he has shown qualities like critical thinking & teamwork. He has an in-depth understanding of frontend development. If you are looking up for the above qualities, he would be a true asset to the company.",
+    name: "Abhilakshya Dobhal",
+    title: "Software Engineer, Novonaut",
+    src: "/abhi.jpeg"
+  },
+  {
+    quote:
+      "I have had the pleasure of working with Koustav on Livly projects, where they consistently demonstrated qualities such as critical thinking and teamwork. He have a deep understanding of frontend development and excel in creating user-friendly interfaces.",
+    name: "Carlos",
+    title: "Software Engineer - Backend, Livly",
+    src: "/carlos.jpeg"
+  },
+  {
+    quote:
+      "I am thoroughly impressed with the Young Indian website! The frontend design, crafted by Koustav, is sleek, modern, and user-friendly. The layout is intuitive, making navigation effortless, while the aesthetics are visually appealing and cohesive.",
+    name: "Baibhav Agarwal",
+    title: "Chairman of Young Indian, Kolkata",
+    src: "/baibhab.jpeg"
+  },
+  {
+    quote:
+      "I have collaborated with Koustav on numerous projects, and he consistently demonstrates dedication to his work. He is a fast learner and an effective problem solver. I believe he would be a valuable asset to the company and highly recommend him.",
+    name: "Saurav Jain",
+    title: "Data Engineer, IG group",
+    src: "/saurav.jpeg"
+  }
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    cta: "https://github.com/Prathameshzad"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    cta: "https://x.com/ZadPrathamesh"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    cta: "https://www.linkedin.com/in/prathamesh-zad-a0930a1a8/?locale=en_UShttps://www.linkedin.com/in/prathamesh-zad-a0930a1a8/"
+  }
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "React",
+    img: "/React.webp"
+  },
+  {
+    id: 2,
+    name: "Next.js",
+    img: "/next.png"
+  },
+  {
+    id: 3,
+    name: "Tailwind CSS",
+    img: "/tailwind.svg"
+  },
+  {
+    id: 4,
+    name: "Aceternity UI",
+    img: "/acernity.webp"
+  },
+  {
+    id: 5,
+    name: "Vercel",
+    img: "/vercel.svg"
+  }
+];
